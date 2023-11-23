eb6d0888278ba3d781298c37fc6c504fe36cdfcc
Herr Turing <admin@nda.software>
2023-11-23 16:46:45 -0300

    Upload for live testing.

---
+++ src/server/action.ts
@@ -0,0 +1,23 @@
+import { type ActionParameters } from './socket';
+
+export default abstract class Action {
+    async prepareAction (): Promise<void> {}
+
+    protected async onCheckPermissions (_parameters: ActionParameters): Promise<void> {}
+
+    protected async onError (_parameters: ActionParameters, err: unknown): Promise<void> {
+        console.error(err);
+    }
+
+    async run (parameters: ActionParameters): Promise<void> {
+        try {
+            await this.onCheckPermissions(parameters);
+
+            await this.onRun(parameters);
+        } catch (err) {
+            await this.onError(parameters, err);
+        }
+    };
+
+    abstract onRun (data: ActionParameters): Promise<void>;
+};
---
+++ src/server/index.ts
@@ -0,0 +1,16 @@
+/* AUTO-GENERATED, DO NOT EDIT MANUALLY */
+import action, * as actionNamed from './action';
+import socket, * as socketNamed from './socket';
+
+export { action };
+export { socket };
+
+export * from './action';
+export * from './socket';
+
+export default {
+    action,
+    socket,
+    ...actionNamed,
+    ...socketNamed
+};
