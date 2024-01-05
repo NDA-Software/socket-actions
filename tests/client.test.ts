68fc0ce8f9ac5e9941b1433fe79aaaa2f13eabbd
Herr Turing <admin@nda.software>
2024-01-05 19:40:08 -0300

    Test file for client class.

---
+++ tests/client.test.ts
@@ -0,0 +1,102 @@
+import Client from '../src/client';
+
+import { Socket, type onError as onErrorType, type onAuth as onAuthType } from '../src';
+
+import ShootLightning from '../mockActions/shootLightning';
+import Hello from '../mockActions/hello';
+
+const actions = {
+    shootLightning: new ShootLightning(),
+    hello: new Hello()
+};
+
+let socketServer: Socket | null = null;
+
+const onAuth: onAuthType = async (_, data) => {
+    if (data.toString() !== '12345')
+        throw new Error('Invalid login!');
+};
+
+const onError: onErrorType = async (socket) => {
+    socket.send('oi');
+};
+
+beforeAll(() => {
+    socketServer = new Socket({
+        actions,
+        port: 3003,
+        onAuth,
+        onError
+    });
+});
+
+describe('Client:', () => {
+    test('Testing authentication...', (done) => {
+        let count = 0;
+        const onMessage = async (message: MessageEvent | string): Promise<void> => {
+            switch (count) {
+                case 0:
+                    expect(message).toBe('Authenticated');
+                    break;
+
+                case 1:
+                    expect(message).toBe('Failed Authentication');
+
+                    con1.sendAction('shootLightning');
+
+                    break;
+
+                case 2:
+                    expect((message as MessageEvent).data).toBe('I am firing my lazer! DAAAAAAAAAAAAAAAH!');
+
+                    con2.sendAction('shootLightning');
+                    break;
+
+                case 3:
+                    expect(message).toBe('Failed Authentication');
+
+                    expect(con1.isAuthenticated).toBe(true);
+                    expect(con2.isAuthenticated).toBe(false);
+
+                    con1.close();
+                    con2.close();
+
+                    done();
+                    break;
+            }
+
+            count++;
+        };
+
+        const defaultOptions = {
+            onAuthSuccess: onMessage, onMessage, onAuthFailure: onMessage
+        };
+
+        const con1 = new Client('ws://localhost:3003', { authentication: '12345', ...defaultOptions });
+        const con2 = new Client('ws://localhost:3003', { authentication: '67890', ...defaultOptions });
+    });
+
+    test('Testing sendAction...', (done) => {
+        const onAuthSuccess = async (): Promise<void> => {
+            con.sendAction('hello', { name: 'Test' });
+        };
+
+        const onMessage = async (message: MessageEvent): Promise<void> => {
+            expect(message.data).toBe('Hello Test!');
+
+            con.close();
+
+            done();
+        };
+
+        const con = new Client('ws://localhost:3003', {
+            authentication: '12345',
+            onMessage,
+            onAuthSuccess
+        });
+    });
+});
+
+afterAll(() => {
+    socketServer?.close();
+});
