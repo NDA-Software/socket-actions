dea1fe267aabc3088cae53dd59ad8aa0cf03de8b
Herr Turing <admin@nda.software>
2024-12-27 15:11:28 -0300

    Fix on how the packages were organized during build.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,18 +1,18 @@
 /* eslint-disable @typescript-eslint/explicit-function-return-type */
 
-import { existsSync, mkdirSync, writeFileSync, readFileSync } from 'fs';
+import { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 
-import nodeResolve from '@rollup/plugin-node-resolve';
-import typescript from '@rollup/plugin-typescript';
-import json from '@rollup/plugin-json';
+import nodeResolve from "@rollup/plugin-node-resolve";
+import typescript from "@rollup/plugin-typescript";
+import json from "@rollup/plugin-json";
 
-import copy from 'rollup-plugin-copy';
-import del from 'rollup-plugin-delete';
-import indexer from 'rollup-plugin-indexer';
-import { dts } from 'rollup-plugin-dts';
+import copy from "rollup-plugin-copy";
+import del from "rollup-plugin-delete";
+import indexer from "rollup-plugin-indexer";
+import { dts } from "rollup-plugin-dts";
 
-const packageJson = JSON.parse(readFileSync('./package.json'));
-const tsConfig = JSON.parse(readFileSync('./tsconfig.json'));
+const packageJson = JSON.parse(readFileSync("./package.json"));
+const tsConfig = JSON.parse(readFileSync("./tsconfig.json"));
 
 const applyCustomization = (defaultConfig, customConfig) => {
     let output = defaultConfig.output;
@@ -20,7 +20,7 @@ const applyCustomization = (defaultConfig, customConfig) => {
     if (customConfig.output?.[0] !== undefined) {
         output[0] = {
             ...output[0],
-            ...customConfig.output[0]
+            ...customConfig.output[0],
         };
 
         if (customConfig.output[1] !== undefined) {
@@ -28,7 +28,7 @@ const applyCustomization = (defaultConfig, customConfig) => {
 
             output = [
                 ...output,
-                ...customConfig.output
+                ...customConfig.output,
             ];
         }
     }
@@ -36,29 +36,31 @@ const applyCustomization = (defaultConfig, customConfig) => {
     const plugins = [
         ...sharedConfigs.plugins,
         ...customConfig.plugins,
-        ...defaultConfig.plugins
+        ...defaultConfig.plugins,
     ];
 
     return {
         ...sharedConfigs,
         ...customConfig,
         output,
-        plugins
+        plugins,
     };
 };
 
 // #region Mock Data:
-const tempFolder = '.temp';
-if (!existsSync(tempFolder))
+const tempFolder = ".temp";
+if (!existsSync(tempFolder)) {
     mkdirSync(tempFolder);
+}
 
 const mockFile = `${tempFolder}/mock.js`;
-if (!existsSync(mockFile))
-    writeFileSync(mockFile, 'export default () => 42;');
+if (!existsSync(mockFile)) {
+    writeFileSync(mockFile, "export default () => 42;");
+}
 
 const mockConfig = {
-    input: '.temp/mock.js',
-    output: [{ dir: '.temp/' }]
+    input: ".temp/mock.js",
+    output: [{ dir: ".temp/" }],
 };
 // #endregion
 
@@ -69,24 +71,25 @@ const config = [];
 
 const external = [];
 
-for (const key in packageJson.peerDependencies)
+for (const key in packageJson.peerDependencies) {
     external.push(key);
+}
 
 const sharedConfigs = {
-    input: 'src/index.ts',
+    input: "src/index.ts",
     plugins: [
         nodeResolve(),
-        json()
+        json(),
     ],
-    external
+    external,
 };
 
-const hasCjs = exports['.'].require !== undefined;
-const hasEsm = exports['.'].import !== undefined;
+const hasCjs = exports["."].require !== undefined;
+const hasEsm = exports["."].import !== undefined;
 const hasTypes = tsConfig.compilerOptions.declaration === true;
 
 let declaration = hasTypes;
-let declarationDir = declaration ? './dist/types/' : undefined;
+let declarationDir = declaration ? "./dist/types/" : undefined;
 const preserveModules = (Object.keys(exports)).length > 1;
 // #endregion
 
@@ -94,14 +97,14 @@ const preserveModules = (Object.keys(exports)).length > 1;
 const configCjs = {
     output: [{}],
     plugins: [
-        indexer('src', { nameCasing: 'PascalCase', exportMode: 'default' }),
-        indexer(['src/server', 'src/helpers'], { nameCasing: 'PascalCase' })
-    ]
+        indexer("src", { nameCasing: "PascalCase", exportMode: "default" }),
+        indexer(["src/server", "src/helpers"], { nameCasing: "PascalCase" }),
+    ],
 };
 
 const configEsm = {
     output: [{}],
-    plugins: []
+    plugins: [],
 };
 // #endregion
 
@@ -109,25 +112,29 @@ const configEsm = {
 if (hasCjs) {
     const finalConfigCjs = applyCustomization({
         output: [{
-            exports: 'named',
-            dir: 'dist/',
-            format: 'cjs',
+            exports: "named",
+            dir: "dist/",
+            format: "cjs",
             preserveModules,
-            entryFileNames: '[name].cjs'
+            entryFileNames: "[name].cjs",
         }],
         plugins: [
-            typescript({ tsconfig: 'tsconfig.json', declaration, declarationDir })
-        ]
+            typescript({
+                tsconfig: "tsconfig.json",
+                declaration,
+                declarationDir,
+            }),
+        ],
     }, configCjs);
 
     config.push(finalConfigCjs);
 }
 
 if (hasEsm) {
-    let folder = 'dist/';
+    let folder = "dist/";
 
     if (hasCjs) {
-        folder = 'dist/esm/';
+        folder = "dist/esm/";
         declaration = false;
         declarationDir = undefined;
 
@@ -136,37 +143,92 @@ if (hasEsm) {
             plugins: [
                 copy({
                     targets: [
-                        { src: ['dist/*', '!dist/cjs', '!dist/types', '!dist/esm'], dest: 'dist/cjs/' }
-                    ]
-                })
-            ]
+                        {
+                            src: [
+                                "dist/*",
+                                "!dist/cjs",
+                                "!dist/types",
+                                "!dist/esm",
+                            ],
+                            dest: "dist/cjs/",
+                        },
+                        { src: ["dist/cjs/src/*"], dest: "dist/cjs/" },
+                    ],
+                }),
+            ],
+        });
+
+        config.push({
+            ...mockConfig,
+            plugins: [
+                copy({
+                    targets: [
+                        { src: ["dist/cjs/src/*"], dest: "dist/cjs/" },
+                    ],
+                }),
+            ],
         });
 
         config.push({
             ...mockConfig,
             plugins: [
                 del({
-                    targets: ['dist/*', '!dist/cjs', '!dist/types'],
-                    recursive: true
-                })
-            ]
+                    targets: ["dist/*", "!dist/cjs", "!dist/types"],
+                    recursive: true,
+                }),
+            ],
+        });
+
+        config.push({
+            ...mockConfig,
+            plugins: [
+                del({
+                    targets: ["dist/cjs/src"],
+                    recursive: true,
+                }),
+            ],
         });
     }
 
     const finalConfigEsm = applyCustomization({
         output: [{
-            exports: 'named',
+            exports: "named",
             dir: folder,
-            format: 'esm',
+            format: "esm",
             preserveModules,
-            entryFileNames: '[name].mjs'
+            entryFileNames: "[name].mjs",
         }],
         plugins: [
-            typescript({ tsconfig: 'tsconfig.json', declaration, declarationDir })
-        ]
+            typescript({
+                tsconfig: "tsconfig.json",
+                declaration,
+                declarationDir,
+            }),
+        ],
     }, configEsm);
 
     config.push(finalConfigEsm);
+
+    config.push({
+        ...mockConfig,
+        plugins: [
+            copy({
+                targets: [
+                    { src: ["dist/esm/src/*"], dest: "dist/esm/" },
+                ],
+            }),
+        ],
+    });
+
+    config.push({
+        ...mockConfig,
+        plugins: [
+            del({
+                targets: ["dist/esm/src"],
+                recursive: true,
+            }),
+        ],
+    });
 }
 // #endregion
 
@@ -177,50 +239,56 @@ if (hasTypes) {
         plugins: [
             copy({
                 targets: [
-                    { src: 'dist/types/src/*', dest: 'dist/types' }
-                ]
-            })
-        ]
+                    { src: "dist/types/src/*", dest: "dist/types" },
+                ],
+            }),
+        ],
     });
 
     config.push({
-        input: 'dist/types/index.d.ts',
+        input: "dist/types/index.d.ts",
         output: [{
-            file: 'dist/types/index.d.ts',
-            format: 'es'
+            file: "dist/types/index.d.ts",
+            format: "es",
         }],
         plugins: [dts()],
-        external: ['http']
+        external: ["http"],
     });
 
     config.push({
-        input: 'dist/types/server/index.d.ts',
+        input: "dist/types/server/index.d.ts",
         output: [{
-            file: 'dist/types/server.d.ts',
-            format: 'es'
+            file: "dist/types/server.d.ts",
+            format: "es",
         }],
         plugins: [dts()],
-        external: ['http']
+        external: ["http"],
     });
 
     config.push({
-        input: 'dist/types/helpers/index.d.ts',
+        input: "dist/types/helpers/index.d.ts",
         output: [{
-            file: 'dist/types/helpers.d.ts',
-            format: 'es'
+            file: "dist/types/helpers.d.ts",
+            format: "es",
         }],
         plugins: [dts()],
-        external: ['http']
+        external: ["http"],
     });
 
     config.push({
         ...mockConfig,
         plugins: [
             del({
-                targets: ['dist/types/src', 'dist/types/tests', 'dist/types/helpers/', 'dist/types/server', 'dist/types/mockActions'],
-                recursive: true
-            })
-        ]
+                targets: [
+                    "dist/types/src",
+                    "dist/types/tests",
+                    "dist/types/helpers/",
+                    "dist/types/server",
+                    "dist/types/mockActions",
+                ],
+                recursive: true,
+            }),
+        ],
     });
 }
 // #endregion
